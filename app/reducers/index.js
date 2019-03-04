0b977f327687f35c2f9006c2d647b63c14480838
Gonzalo Vitta <gvitta@gmail.com>
2019-03-04 11:11:47 -0300

    refactor actions para bind con API

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,4 +1,4 @@
-import { ADD_ARTICLE, DATA_LOADED, FILTER_POST, ADD_POST } from "../constants/action-types";
+import { ADD_ARTICLE, DATA_LOADED, FILTER_POST, ADD_POST, DEL_POST } from "../constants/action-types";
 import uuidv1 from "uuid";
 const initialState = {
   articles: [{id:1, title:"Hello World"},{id:2, title:"Hello World2"}],
@@ -16,29 +16,29 @@ function rootReducer(state = initialState, action) {
 
   if (action.type === DATA_LOADED) {
     return Object.assign({}, state, {
-      remoteArticles: state.remoteArticles.concat(action.payload).slice(0, 8),
-      showedArticles: state.remoteArticles.concat(action.payload).slice(0,8)
+      remoteArticles: state.remoteArticles.concat(action.payload),
+      showedArticles: state.remoteArticles.concat(action.payload)
     });
   }
 
   if (action.type === FILTER_POST) {
-    console.log(action.searchText);
     return Object.assign({}, state, {
       searchText: action.searchText.searchText,
-      showedArticles: state.remoteArticles.filter( (item) => item.title.includes(action.searchText.searchText))
+      showedArticles: state.remoteArticles.filter( (item) => item.name.includes(action.searchText.searchText))
     });
   }
 
   if (action.type === ADD_POST) {
-    console.log(Object.assign({}, state, {
-      remoteArticles: state.remoteArticles.concat({id: uuidv1(), title:action.newPost.desc}),
-      // showedArticles: state.remoteArticles.filter( (item) => item.title.includes(state.searchText))
-      showedArticles: state.remoteArticles.concat({id: uuidv1(), title:action.newPost.desc})
-    }));
     return Object.assign({}, state, {
-      remoteArticles: state.remoteArticles.concat({id: uuidv1(), title:action.newPost.desc}),
-      // showedArticles: state.remoteArticles.filter( (item) => item.title.includes(state.searchText))
-      showedArticles: state.remoteArticles.concat({id: uuidv1(), title:action.newPost.desc})
+      remoteArticles: state.remoteArticles.concat(action.payload),
+      showedArticles: state.remoteArticles.concat(action.payload)
+    });
+  }
+
+  if (action.type === DEL_POST) {
+    return Object.assign({}, state, {
+      remoteArticles: state.remoteArticles.filter((item) => item.id !== action.payload.id),
+      showedArticles: state.remoteArticles.filter((item) => item.id !== action.payload.id)
     });
   }
   return state;

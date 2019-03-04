df82bded7bd85df6da15225e7281dc8cac5c6715
Gonzalo Vitta <gvitta@gmail.com>
2019-03-04 11:10:48 -0300

    Bind con local API

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,12 +1,12 @@
 // src/js/actions/index.js
-import { ADD_ARTICLE, DATA_LOADED, FILTER_POST, ADD_POST } from "../constants/action-types";
+import { ADD_ARTICLE, DATA_LOADED, FILTER_POST, ADD_POST, DEL_POST } from "../constants/action-types";
 export function addArticle(payload) {
   return { type: ADD_ARTICLE, payload };
 }
 
 export function getData() {
   return function(dispatch) {
-    return fetch("https://jsonplaceholder.typicode.com/posts")
+    return fetch("http://localhost:3000/posts")
       .then(response => response.json())
       .then(json => {
         dispatch({ type: DATA_LOADED, payload: json });
@@ -19,5 +19,28 @@ export function filterPosts(searchText){
 }
 
 export function addPost(newPost) {
-  return { type: ADD_POST, newPost };
+  return function(dispatch) {
+    return fetch("http://localhost:3000/posts", {
+        method: 'POST', 
+        body: JSON.stringify(newPost), // data can be `string` or {object}!
+        headers:{
+          'Content-Type': 'application/json'
+        }
+      }).then(response => response.json())
+      .then(json => {
+        dispatch({ type: ADD_POST, payload: json });
+      });
+  };
+}
+
+export function delPost(post){
+  return function(dispatch) {
+    const url = "http://localhost:3000/posts/"+post
+    return fetch(url, {
+        method: 'DELETE'
+      }).then(response => response.json())
+      .then(json => {
+        dispatch({ type: DEL_POST, payload: json });
+      });
+  };
 }
